0d2b714ad99a32f9da6620daa1756b4f9aed3bbc
mel225 <mini20cat_drumer@outlook.jp>
2018-12-18 22:31:24 +0900

    create function.js

---
+++ docs/function.js
@@ -0,0 +1,24 @@
+(function(){
+
+function getData(url){
+  var begin = url.indexOf("=") + 1;
+  var pb = url.slice(begin);
+
+  var data = [];
+  begin = pb.indexOf("!1d") + 3;
+  var len = pb.slice(begin).indexOf("!");
+  data["syukusyaku"] = pb.substr(begin,len);
+
+  begin = pb.indexOf("!2d") + 3;
+  len = pb.slice(begin).indexOf("!");
+  data["keido"] = pb.substr(begin,len);
+
+  begin = pb.indexOf("!3d") + 3;
+  len = pb.slice(begin).indexOf("!");
+  data["ido"] = pb.substr(begin,len);
+
+  console.log(data);
+  return data;
+}
+
+}) (document);
